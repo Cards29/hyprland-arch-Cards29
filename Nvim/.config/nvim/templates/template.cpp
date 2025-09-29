84b2db79e4a2c7bcccde0df5a93366c4f57f92f9
Rahinur Bin Naushad <rahinur2004@gmail.com>
2025-09-29 23:45:18 +0600

    added ordered set

--- Nvim/.config/nvim/templates/template.cpp
+++ Nvim/.config/nvim/templates/template.cpp
@@ -1,5 +1,15 @@
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
 using namespace std;
+using namespace __gnu_pbds;
+
+// find_by_order(k): returns an iterator to the k-th element (0-indexed)
+// order_of_key(k): returns the number of elements strictly less than k
+template <typename T>
+using ordered_set =
+    tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 
 typedef long long ll;
 typedef pair<ll, ll> pi;
